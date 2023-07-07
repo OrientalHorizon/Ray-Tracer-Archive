170f66f7a5f0dfac0286d4cb2075f33de659242a
OrientalHorizon <iaknoip@outlook.com>
2023-07-07 15:33:20 +0800

    Book1 image14

--- raytracer/src/material.rs
+++ raytracer/src/material.rs
@@ -1,6 +1,6 @@
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
-use crate::vec3::{dot, random_in_unit_sphere, random_unit_vector, reflect, Color3, Vec3};
+use crate::vec3::{dot, random_in_unit_sphere, random_unit_vector, reflect, refract, Color3, Vec3};
 
 pub trait Material {
     fn scatter(
@@ -79,5 +79,41 @@ impl Material for Metal {
         *scattered = Ray::construct(&rec.p, &(reflected + self.fuzz * random_in_unit_sphere()));
         *attenuation = self.albedo;
         dot(&scattered.direction(), &rec.normal) > 0.0
+    }
+}
+
+pub struct Dielectric {
+    ir: f64, // Index of Refraction
+}
+
+impl Dielectric {
+    // pub fn new() -> Self {
+    //     Self { ir: 1.0 }
+    // }
+    pub fn construct(ir: f64) -> Self {
+        Self { ir }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Vec3,
+        scattered: &mut Ray,
+    ) -> bool {
+        *attenuation = Vec3::construct(&[1.0, 1.0, 1.0]);
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = r_in.direction().unit();
+        let refracted = refract(&unit_direction, &rec.normal, refraction_ratio);
+
+        *scattered = Ray::construct(&rec.p, &refracted);
+        true
     }
 }
