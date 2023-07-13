2a2af847e605a39ce9593d6d7125390a32413500
OrientalHorizon <iaknoip@outlook.com>
2023-07-13 09:43:23 +0800

    Book2 image12

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -195,7 +195,7 @@ pub fn two_perlin_spheres() -> HittableList {
 }
 
 fn main() {
-    let path = std::path::Path::new("output/book2/image11.jpg");
+    let path = std::path::Path::new("output/book2/image12.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
